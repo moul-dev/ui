97ad29a240e195f9f1c73b7c8264a03f6a8440b3
Phearak S. Tha <t.sophearak@gmail.com>
2026-06-22 11:38:59 +0700

    [docs] add get started

--- docs/src/components/mdx.tsx
+++ docs/src/components/mdx.tsx
@@ -1,6 +1,9 @@
 import { Button } from '@moul-dev/ui'
 import defaultMdxComponents from 'fumadocs-ui/mdx'
 import type { MDXComponents } from 'mdx/types'
+import { Card, Cards } from 'fumadocs-ui/components/card'
+import { Step, Steps } from 'fumadocs-ui/components/steps'
+import { Tab, Tabs } from 'fumadocs-ui/components/tabs'
 import { ComponentPreview } from './component-preview'
 import { AlertDialogDemo, ModalDemo, ToastDemo, FormDemo } from './demos'
 
@@ -8,6 +11,12 @@ export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
     Button,
+    Card,
+    Cards,
+    Step,
+    Steps,
+    Tab,
+    Tabs,
     ComponentPreview,
     AlertDialogDemo,
     ModalDemo,
