e68ec3589dc2bc603266da6a7a255613c204c2f5
Phearak S. Tha <t.sophearak@gmail.com>
2026-08-25 21:07:25 +0700

    Bump UI version

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,5 +1,5 @@
 // Entrypoint for @moul-dev/ui component library
-export const version = '2026.08.22'
+export const version = '2026.08.25'
 
 export { Alert } from './components/Alert'
 export type { AlertProps, AlertVariant } from './components/Alert/Alert'
